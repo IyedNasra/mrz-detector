08e93b8283bd36ff1fe1dfd33c0b66a4794ff523
IyedNasra <iyed.nasra.in@gmail.com>
2023-08-28 10:52:37 +0100

    deleted folder

--- mrz-parser/index.js
+++
@@ -1,7 +0,0 @@
-
-const parse = require('mrz').parse;
-
-let mrz = ['P<GBRTHATCHER<<ROSS<<<<<<<<<<<<<<<<<<<<<<<<<', '2841464979TUN4904265M1601013<<<<<<<<<<<<<<04'];
-
-var result = parse(mrz);
-console.log(result);
