576adb19e9abe988fd356eacceb11ed2b47fa3de
IyedNasra <iyed.nasra.in@gmail.com>
2023-08-30 10:33:45 +0100

    everything works.

--- mrz-detection-master/app.py
+++ mrz-detection-master/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, redirect, url_for
 import os
+from imagetomrz import ExtractData
+from parsemrz import parse
 
 app = Flask(__name__)
 
-BASEDIR = os.getcwd()
 UPLOAD_FOLDER = 'img'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
@@ -13,23 +14,51 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_image():
+    ima_path = None  # Initialize the variable
+
     if request.method == 'POST':
         # Check if the file is an image
         file = request.files['image']
         if file and allowed_file(file.filename):
             # Move the file to the upload directory
             filename = file.filename
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
-
-            print('The image has been uploaded successfully!')
-
-            return redirect(url_for('upload_image'))
-
+            relative_path = os.path.join(UPLOAD_FOLDER, filename)
+            absolute_path = os.path.abspath(relative_path)
+            file.save(absolute_path)
+            ima_path = absolute_path
+            list = ExtractData(ima_path)
+            l = parse(list)
+            print(l)
+            
     return '''
-        <form action="" method="post" enctype="multipart/form-data">
-            <input type="file" name="image">
-            <input type="submit" value="Upload">
-        </form>
+        <!doctype html>
+        <html>
+        <head>
+            <title>Image Upload</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    text-align: center;
+                }
+                .upload-form {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    margin-top: 50px;
+                }
+                input[type="file"] {
+                    margin-bottom: 20px;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Upload an Image</h1>
+            <form class="upload-form" action="" method="post" enctype="multipart/form-data">
+                <input type="file" name="image">
+                <input type="submit" value="Upload">
+            </form>
+        </body>
+        </html>
     '''
 
 if __name__ == '__main__':
